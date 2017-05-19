321c5e1f68b28bd174cc654b4b75aae9272d5e7d
ludi940205 <ludi940205@gmail.com>
2017-05-19 15:03:30 -0700

    proj2

--- nachos/test/creat.c
+++ nachos/test/creat.c
@@ -1,13 +1,8 @@
 #include "syscall.h"
 #include "stdlib.h"
 
-int main(int argc,char *argv[]) {
-    if (argc == 0)
-        return 0;
-    if (argc == 1)
-        return 0;
-    char fileName[100];
-    strcpy(fileName, argv[1]);
+int main() {
+    char* fileName = "test.txt";
     creat(fileName);
 
     return 0;
