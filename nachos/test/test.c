e730541a90b631fc5bc99a93199ecc2215877a38
Di Lu <dil051@ieng6-251.ucsd.edu>
2017-05-20 00:00:01 -0700

    test for proj2

--- nachos/test/test.c
+++ nachos/test/test.c
@@ -4,6 +4,30 @@
 
 int main() {
 	printf("Hello world!\n");
+	
+	int newFile, oldFile;
+	char* buffer;
+	
+	printf("Creating file\n");
+	newFile = creat("TestFile.txt");
+	
+	printf("Writing file\n");
+	write(newFile, "Hello world!!!\n", 15);
+	
+	printf("Closing file\n");
+	close(newFile);
+
+	printf("Opening file\n");
+	oldFile = open("TestFile.txt");
+
+	printf("Reading file\n");
+	read(oldFile, buffer, 15);
+	
+	printf(buffer);
+	
+	printf("Closing file\n");
+	close(oldFile);
+
 	return 0;
 }
 
