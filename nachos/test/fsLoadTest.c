27ec5959de690ef5323a9e232281334f024e12f3
Di Lu <dil051@ieng6-251.ucsd.edu>
2017-05-20 23:27:46 -0700

    test for proj2

--- nachos/test/fsLoadTest.c
+++ nachos/test/fsLoadTest.c
@@ -6,7 +6,7 @@ int main() {
     int files[20];
     int flag;
 
-    files[0] = creat("testFiles/f1.txt");
+    files[0] = creat("f1.txt");
     printf("creating file %d, %d\n", 1, files[0]);
 
     flag = close(files[0]);
