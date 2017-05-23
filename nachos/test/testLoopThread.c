0788bf19309d03ac1647fdad2fe2c0e20fbc33bd
ludi940205 <ludi940205@gmail.com>
2017-05-22 23:58:21 -0700

    project 2

--- nachos/test/testLoopThread.c
+++ nachos/test/testLoopThread.c
@@ -8,7 +8,7 @@ int main(int argc, char* argv[]) {
     if (argc != 1)
         exit(1);
 
-    printf("Inside thread %s\n", argv[0]);
+//    printf("Inside thread %s\n", argv[0]);
     for (i = 0; i < 1000000; i++);
     printf("Finished thread %s\n", argv[0]);
 
