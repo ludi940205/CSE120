8d5c46cc1f3fbc0a3a0665ea686458b155640c02
ludi940205 <ludi940205@gmail.com>
2017-05-23 18:30:53 -0700

    proj2 testing

--- nachos/test/isprmgr.c
+++ nachos/test/isprmgr.c
@@ -11,17 +11,15 @@
  *   $BC,EPT     4/21/2014 - initial release
  **********************************************************************/
 #include "stdio.h"
+#include "stdlib.h"
 
 #define NULL        0
 #define NUMVARS     8
 #define NAN         (0xEFFFFFFF)
 #define MAXARGC     20
 #define MAXPROCESS  9
-#define LOG         printf
 
-void log(char *format, ...);
 void route(int, char);
-int  atoi(const char *str);
 
 int pid[10];                    /* array to store pid                                    */
 char *executable;               /* executable file name for exec()                       */
@@ -54,13 +52,13 @@ int main(int argc, char *argv[]) {
     if(argc > 1)
         variation = atoi(argv[1]);
 
-    LOG("++ISPRMGR: ARG[1] is %d \n", variation);
+    printf("++ISPRMGR: ARG[1] is %d \n", variation);
 
     if (variation) {
         route(variation, dbg_flag);
     }
     else {
-        LOG("++ISPRMGR Run all the variations\n");
+        printf("++ISPRMGR Run all the variations\n");
         for (i=1; i <= NUMVARS; i++)
             route(i, dbg_flag);
     }
@@ -86,23 +84,23 @@ void route(int variation, char dbg_flag)
             /* process immediately.                                      */
             /*                                                           */
             /*************************************************************/
-            LOG("++ISPRMGR VAR1: [STARTED]\n");
+            printf("++ISPRMGR VAR1: [STARTED]\n");
             executable = "exittest.coff";
             _argv[0] = executable;
             _argv[1] = NULL;
             _argc = 1;
-            LOG("++ISPRMGR VAR1: exec %s\n", executable);
+            printf("++ISPRMGR VAR1: exec %s\n", executable);
             pid[0] = exec(executable, _argc, _argv);
-            LOG("++ISPRMGR VAR1: Child process id is %d\n", pid[0]);
+            printf("++ISPRMGR VAR1: Child process id is %d\n", pid[0]);
 
             executable = "loopawhile.coff";
             _argv[0] = executable;
             _argv[1] = NULL;
             _argc = 1;
-            LOG("++ISPRMGR VAR1: exec %s\n", executable);
+            printf("++ISPRMGR VAR1: exec %s\n", executable);
             pid[0] = exec(executable, _argc, _argv);
-            LOG("++ISPRMGR VAR1: Child process id is %d\n", pid[0]);
-            LOG("++ISPRMGR VAR1: [ENDED] SUCCESS\n");
+            printf("++ISPRMGR VAR1: Child process id is %d\n", pid[0]);
+            printf("++ISPRMGR VAR1: [ENDED] SUCCESS\n");
             /* FIX ME [thinkhy 4/27/2014] */
             /* The second LOG will cause this program to get hung */
             /* LOG("++ProjectII TaskIII VAR1: FAILED"); */
@@ -118,7 +116,7 @@ void route(int variation, char dbg_flag)
             /*                                                           */
             /*************************************************************/
             // log("++ProjectII TaskIII VAR2");
-            LOG("++ISPRMGR VAR2: [STARTED]\n");
+            printf("++ISPRMGR VAR2: [STARTED]\n");
             executable = "cp.coff";
             _argv[0] = executable;
             _argv[1] = "cat.coff";
@@ -127,20 +125,20 @@ void route(int variation, char dbg_flag)
 
             for (i = 0; i <  MAXPROCESS; i++) {
                 // LOG("before");
-                LOG("++ISPRMGR VAR2: exec %s\n", executable);
+                printf("++ISPRMGR VAR2: exec %s\n", executable);
                 pid[i] = exec(executable, _argc, _argv);
-                LOG("++ISPRMGR VAR2: Get PID %d after exec cp.coff\n", pid[i]);
+                printf("++ISPRMGR VAR2: Get PID %d after exec cp.coff\n", pid[i]);
             
                 for (j = 0; j < i; ++j)  {
                     if (pid[j] == pid[i]) {
-                        LOG("++ISPRMGR VAR2: FAILED, pid[%d] equals pid[%d]\n",
+                        printf("++ISPRMGR VAR2: FAILED, pid[%d] equals pid[%d]\n",
                                     pid[j], pid[i]);
                         exit(-1);
                     }
                 }
             }
 
-            LOG("++ISPRMGR VAR2: [ENDED] SUCCESS\n");
+            printf("++ISPRMGR VAR2: [ENDED] SUCCESS\n");
 
             break;
 
@@ -151,24 +149,24 @@ void route(int variation, char dbg_flag)
             /* Variation 3:  tests your syscall join to a child          */
             /*                                                           */
             /*************************************************************/
-            LOG("++ISPRMGR VAR3: [STARTED]\n");
+            printf("++ISPRMGR VAR3: [STARTED]\n");
             
             executable = "exittest.coff";
             _argv[0] = executable;
             _argv[1] = NULL;
             _argc = 1;
-            LOG("++ISPRMGR VAR3: exec %s\n", executable);
+            printf("++ISPRMGR VAR3: exec %s\n", executable);
             pid[0] = exec(executable, _argc, _argv);
-            LOG("++ISPRMGR VAR3: Child process id is %d\n", pid[0]);
-            LOG("++ISPRMGR VAR3: Issue join to get exit status of chile process\n", pid[0]);
+            printf("++ISPRMGR VAR3: Child process id is %d\n", pid[0]);
+            printf("++ISPRMGR VAR3: Issue join to get exit status of chile process\n", pid[0]);
             retval = join(pid[0], &exitstatus);
             if (retval == 0) {
-                LOG("++ISPRMGR VAR3: join successfully, exit status is %d\n", exitstatus);
-                LOG("++ISPRMGR VAR3: [ENDED] SUCCESS\n");
+                printf("++ISPRMGR VAR3: join successfully, exit status is %d\n", exitstatus);
+                printf("++ISPRMGR VAR3: [ENDED] SUCCESS\n");
             }
             else {
-                LOG("++ISPRMGR VAR3: return value of join is %d\n", retval);
-                LOG("++ISPRMGR VAR3: [ENDED] FAIL\n");
+                printf("++ISPRMGR VAR3: return value of join is %d\n", retval);
+                printf("++ISPRMGR VAR3: [ENDED] FAIL\n");
             }
             
             break;
@@ -179,18 +177,18 @@ void route(int variation, char dbg_flag)
             /* Variation 4:  tests exec with error arguments: bad file name)         */
             /*                                                                       */
             /*************************************************************************/
-            LOG("++ISPRMGR VAR4: [STARTED]\n");
-            LOG("++ISPRMGR VAR4: invoke exec with nonextent executable\n");
+            printf("++ISPRMGR VAR4: [STARTED]\n");
+            printf("++ISPRMGR VAR4: invoke exec with nonextent executable\n");
             executable = "inexistent.coff";
             _argv[0] = executable;
             _argv[1] = NULL;
             _argc = 1; 
-            LOG("++ISPRMGR VAR4: exec %s\n", executable);
+            printf("++ISPRMGR VAR4: exec %s\n", executable);
             retval = exec(executable, _argc, _argv);
             if (retval == -1)
-                LOG("++ISPRMGR VAR4: [END] SUCCESS\n");
+                printf("++ISPRMGR VAR4: [END] SUCCESS\n");
             else
-                LOG("++ISPRMGR VAR4: [END] FAIL\n");
+                printf("++ISPRMGR VAR4: [END] FAIL\n");
 
             break;
 
@@ -200,35 +198,35 @@ void route(int variation, char dbg_flag)
             /* Variation 5:  tests exec with error arguments: unmatched argc         */
             /*                                                                       */
             /*************************************************************************/
-            LOG("++ISPRMGR VAR5: [STARTED]\n");
-            LOG("++ISPRMGR VAR5: invoke exec with unmatched argc\n");
+            printf("++ISPRMGR VAR5: [STARTED]\n");
+            printf("++ISPRMGR VAR5: invoke exec with unmatched argc\n");
             executable = "exittest.coff";
             _argv[0] = executable;
             _argv[1] = NULL;
             _argc = 100; 
-            LOG("++ISPRMGR VAR5: exec %s\n", executable);
+            printf("++ISPRMGR VAR5: exec %s\n", executable);
             retval = exec(executable, _argc, _argv);
             if (retval != -1) {
-                LOG("++ISPRMGR VAR5: [END] FAIL\n");
+                printf("++ISPRMGR VAR5: [END] FAIL\n");
                 break;
             }
 
             /* TODO; figure out this case */
-            LOG("++ISPRMGR VAR5:"
+            printf("++ISPRMGR VAR5:"
                     " invoke exec with unmatched argc when argv varies\n");
             executable = "exittest.coff";
             _argv[0] = executable;
             _argv[1] = "cat.coff";
             _argv[2] = "cat1.coff";
             _argc = 2; 
-            LOG("++ISPRMGR VAR5: exec %s\n", executable);
+            printf("++ISPRMGR VAR5: exec %s\n", executable);
             retval = exec(executable, _argc, _argv);
             if (retval != 0) {
-                LOG("++ISPRMGR VAR5: Retval of exec is %d \n", retval);
-                LOG("++ISPRMGR VAR5: [END] SUCCESS\n");
+                printf("++ISPRMGR VAR5: Retval of exec is %d \n", retval);
+                printf("++ISPRMGR VAR5: [END] SUCCESS\n");
             }
             else
-                LOG("++ISPRMGR VAR5: [END] FAIL\n");
+                printf("++ISPRMGR VAR5: [END] FAIL\n");
 
             break;
 
@@ -239,23 +237,23 @@ void route(int variation, char dbg_flag)
             /* Var 6 : tests your syscall join to a non-child                        */
             /*                                                                       */
             /*************************************************************************/
-            LOG("++ISPRMGR VAR6: [STARTED]\n");
-            LOG("++ISPRMGR VAR6: Issue join to a non-child with pid=0\n");
+            printf("++ISPRMGR VAR6: [STARTED]\n");
+            printf("++ISPRMGR VAR6: Issue join to a non-child with pid=0\n");
             
             retval = join(0, &exitstatus);
             if (retval == 0) {
-                LOG("++ISPRMGR VAR6: [ENDED] FAIL\n");
+                printf("++ISPRMGR VAR6: [ENDED] FAIL\n");
                 break;
             }
 
-            LOG("++ISPRMGR VAR6: Issue join to myself with pid=1\n");
+            printf("++ISPRMGR VAR6: Issue join to myself with pid=1\n");
             retval = join(1, &exitstatus);
             if (retval == 0) {
-                LOG("++ISPRMGR VAR6: [ENDED] FAIL\n");
+                printf("++ISPRMGR VAR6: [ENDED] FAIL\n");
                 break;
             }
 
-            LOG("++ISPRMGR VAR6: [ENDED] SUCCESS\n");
+            printf("++ISPRMGR VAR6: [ENDED] SUCCESS\n");
         }
 
         case 7:
@@ -264,33 +262,33 @@ void route(int variation, char dbg_flag)
             /* Var 7 : tests your syscall join to be invoked more than once          */
             /*                                                                       */
             /*************************************************************************/
-            LOG("++ISPRMGR VAR7: [STARTED]\n");
-            LOG("++ISPRMGR VAR7: test your syscall join to be invoked more than once\n");
+            printf("++ISPRMGR VAR7: [STARTED]\n");
+            printf("++ISPRMGR VAR7: test your syscall join to be invoked more than once\n");
             
             executable = "exittest.coff";
             _argv[0] = executable;
             _argv[1] = NULL;
             _argc = 1;
-            LOG("++ISPRMGR VAR7: exec %s\n", executable);
+            printf("++ISPRMGR VAR7: exec %s\n", executable);
             pid[0] = exec(executable, _argc, _argv);
-            LOG("++ISPRMGR VAR7: Child process id is %d\n", pid[0]);
-            LOG("++ISPRMGR VAR7: Issue join to get exit status of chile process\n");
+            printf("++ISPRMGR VAR7: Child process id is %d\n", pid[0]);
+            printf("++ISPRMGR VAR7: Issue join to get exit status of chile process\n");
             retval = join(pid[0], &exitstatus);
             if (retval != 0) {
-                LOG("++ISPRMGR VAR7: [ENDED] FAIL\n");
+                printf("++ISPRMGR VAR7: [ENDED] FAIL\n");
                 break;
             }
-            LOG("++ISPRMGR VAR7: first time invoke join successfully\n");
+            printf("++ISPRMGR VAR7: first time invoke join successfully\n");
 
-            LOG("++ISPRMGR VAR7: Issue join again to get exit status of chile process\n", pid[0]);
+            printf("++ISPRMGR VAR7: Issue join again to get exit status of chile process\n", pid[0]);
             retval = join(pid[0], &exitstatus);
             if (retval == 0) {
-                LOG("++ISPRMGR VAR7: [ENDED] FAILED to join process %d\n", pid[0]);
+                printf("++ISPRMGR VAR7: [ENDED] FAILED to join process %d\n", pid[0]);
                 break;
             }
-            LOG("++ISPRMGR VAR7: failed to invoke join second time as exptected\n");
+            printf("++ISPRMGR VAR7: failed to invoke join second time as exptected\n");
 
-            LOG("++ISPRMGR VAR7: [ENDED] SUCCESS\n");
+            printf("++ISPRMGR VAR7: [ENDED] SUCCESS\n");
 
             break;
 
@@ -301,30 +299,30 @@ void route(int variation, char dbg_flag)
             /* that caused unhandled exception                                       */
             /*                                                                       */
             /*************************************************************************/
-            LOG("++ISPRMGR VAR8: [STARTED]\n");
-            LOG("++ISPRMGR VAR8: tests syscall join to a child that caused unhandled exception\n");
+            printf("++ISPRMGR VAR8: [STARTED]\n");
+            printf("++ISPRMGR VAR8: tests syscall join to a child that caused unhandled exception\n");
             
             executable = "exception.coff";
             _argv[0] = executable;
             _argv[1] = NULL;
             _argc = 1; 
-            LOG("++ISPRMGR VAR8: exec %s\n", executable);
+            printf("++ISPRMGR VAR8: exec %s\n", executable);
             pid[0] = exec(executable, _argc, _argv);
             if (pid[0] < 0) {
-                LOG("++ISPRMGR VAR8: [ENDED] FAIL to invoke exec\n");
+                printf("++ISPRMGR VAR8: [ENDED] FAIL to invoke exec\n");
                 break;
             }
 
-            LOG("++ISPRMGR VAR8: Issue join to child with pid=%d\n", pid[0]);
+            printf("++ISPRMGR VAR8: Issue join to child with pid=%d\n", pid[0]);
             retval = join(pid[0], &exitstatus);
             if (exitstatus == 0) {
-                LOG("++ISPRMGR VAR8: Issue join successfully, but expect a failure at here\n");
-                LOG("++ISPRMGR VAR8: [ENDED] FAIL\n");
+                printf("++ISPRMGR VAR8: Issue join successfully, but expect a failure at here\n");
+                printf("++ISPRMGR VAR8: [ENDED] FAIL\n");
                 break;
             }
             else {
-                LOG("++ISPRMGR VAR8: failed to issue join as expected\n");
-                LOG("++ISPRMGR VAR8: [ENDED] SUCCESS\n");
+                printf("++ISPRMGR VAR8: failed to issue join as expected\n");
+                printf("++ISPRMGR VAR8: [ENDED] SUCCESS\n");
             }
            
             break; 
@@ -346,29 +344,3 @@ void route(int variation, char dbg_flag)
 
 
 }
-
-
-void log(char *format, ...) {
-     va_list ap;
-     va_start(ap,format);
- 
-     vprintf(format, ap);
- 
-     va_end(ap);
-}
-
-int atoi(const char *str) {
-    if (str == NULL)
-        return NAN;
-
-    int sum = 0;
-    int i = 0;
-    while(str[i] != NULL) {
-        sum = sum*10 + (str[i]-'0');
-        i++;
-    }
-
-    return sum;
-}
-
-
