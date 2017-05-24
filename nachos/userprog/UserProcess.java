5b56ff88aa565f773fd8acaca4c1ace5177b3db6
ludi940205 <ludi940205@gmail.com>
2017-05-23 17:27:47 -0700

    test autograder

--- nachos/userprog/UserProcess.java
+++ nachos/userprog/UserProcess.java
@@ -630,25 +630,25 @@ public class UserProcess {
 		switch (syscall) {
 			case syscallHalt:
 				return handleHalt();
-			case syscallExit:
-				handleExit(a0);
-				return 0;
-			case syscallExec:
-				return handleExec(a0, a1, a2);
-			case syscallJoin:
-				return handleJoin(a0, a1);
-			case syscallCreate:
-				return handleCreate(a0);
-			case syscallOpen:
-				return handleOpen(a0);
-			case syscallRead:
-				return handleRead(a0, a1, a2);
-			case syscallWrite:
-				return handleWrite(a0, a1, a2);
-			case syscallClose:
-				return handleClose(a0);
-			case syscallUnlink:
-				return handleUnlink(a0);
+//			case syscallExit:
+//				handleExit(a0);
+//				return 0;
+//			case syscallExec:
+//				return handleExec(a0, a1, a2);
+//			case syscallJoin:
+//				return handleJoin(a0, a1);
+//			case syscallCreate:
+//				return handleCreate(a0);
+//			case syscallOpen:
+//				return handleOpen(a0);
+//			case syscallRead:
+//				return handleRead(a0, a1, a2);
+//			case syscallWrite:
+//				return handleWrite(a0, a1, a2);
+//			case syscallClose:
+//				return handleClose(a0);
+//			case syscallUnlink:
+//				return handleUnlink(a0);
 
 
 		default:
