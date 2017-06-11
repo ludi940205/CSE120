81776e2611d2769600e3937ef18ef0b52ab9cdd9
ludi940205 <ludi940205@gmail.com>
2017-06-10 17:40:30 -0700

    test for proj3

--- nachos/test/testRW5.c
+++ nachos/test/testRW5.c
@@ -11,21 +11,43 @@ void assertMsg(int arg, char* msg) {
 }
 
 int main() {
-    char* executable;
+	char* executable;
     char* argv[3];
-    int pid, argc, i;
+    int pid[MAXPROCESS], argc, i, j, exitstatus;
 
     executable = "cp.coff";
     argv[0] = executable;
-    argv[1] = "write.out";
-    argv[2] = "write_copy0.out";
+    argv[1] = "syscall.h";
+    argv[2] = "syscall_copy.h";
     argc = 3;
 
-    for (i = 0; i < 5; i++) {
-        argv[2][10] = i + '0';
-        pid = exec(executable, argc, argv);
-        assertMsg(pid != -1, "MULTI THREAD READ WRITE TEST: Unable to exec cp.coff\n");
+    for (i = 0; i <  MAXPROCESS; i++) {
+        pid[i] = exec(executable, argc, argv);
+        join(pid[i], &exitstatus);
+        assertMsg(pid[i] != -1, "PID TEST: Unable to exec cp.coff\n");
+    
+        for (j = 0; j < i; ++j)
+            assertMsg(pid[j] == -1 || pid[j] != pid[i], "PID TEST: pid equals");
     }
+
+    printf("PID TEST SUCCESS\n");
+
+
+    // char* executable;
+    // char* argv[3];
+    // int pid, argc, i;
+
+    // executable = "cp.coff";
+    // argv[0] = executable;
+    // argv[1] = "write.out";
+    // argv[2] = "write_copy0.out";
+    // argc = 3;
+
+    // for (i = 0; i < 5; i++) {
+    //     argv[2][10] = i + '0';
+    //     pid = exec(executable, argc, argv);
+    //     assertMsg(pid != -1, "MULTI THREAD READ WRITE TEST: Unable to exec cp.coff\n");
+    // }
 
     // executable = "cat.coff";
     // argv[0] = executable;
@@ -38,5 +60,5 @@ int main() {
     //     assertMsg(pid != -1, "MULTI THREAD READ WRITE TEST: Unable to exec cat.coff\n");
     // }
 
-    printf("MULTI THREAD READ WRITE TEST SUCCESS\n");
+    // printf("MULTI THREAD READ WRITE TEST SUCCESS\n");
 }
