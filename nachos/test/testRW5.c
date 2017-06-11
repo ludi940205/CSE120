b8c51d57a649a8a8453b9dcc4bf56c713edb5dc9
ludi940205 <ludi940205@gmail.com>
2017-06-10 17:16:28 -0700

    test for proj3

--- nachos/test/testRW5.c
+++ nachos/test/testRW5.c
@@ -1,7 +1,7 @@
 #include "stdio.h"
 
 #define NULL        0
-#define MAXPROCESS  9
+#define MAXPROCESS  19
 
 void assertMsg(int arg, char* msg) {
     if (arg == 0) {
@@ -11,5 +11,32 @@ void assertMsg(int arg, char* msg) {
 }
 
 int main() {
-	
+    char* executable;
+    char* argv[3];
+    int pid, argc, i;
+
+    executable = "cp.coff";
+    argv[0] = executable;
+    argv[1] = "write.out";
+    argv[2] = "write_copy0.out";
+    argc = 3;
+
+    for (i = 0; i < 5; i++) {
+        argv[2][10] = i + '0';
+        pid = exec(executable, argc, argv);
+        assertMsg(pid != -1, "MULTI THREAD READ WRITE TEST: Unable to exec cp.coff\n");
+    }
+
+    executable = "cat.coff";
+    argv[0] = executable;
+    argv[1] = "write_copy0.out";
+    argc = 2;
+
+    for (i = 0; i < 5; i++) {
+        argv[2][10] = i + '0';
+        pid = exec(executable, argc, argv);
+        assertMsg(pid != -1, "MULTI THREAD READ WRITE TEST: Unable to exec cat.coff\n");
+    }
+
+    printf("MULTI THREAD READ WRITE TEST SUCCESS\n");
 }
